1b3f775e70cd518e74d61bf81aea49a6f50e51d8
jjalseu <cjfwns966@gmail.com>
2024-12-19 20:46:02 +0900

    - WebVIew

--- Playground/ContentView.swift
+++ Playground/ContentView.swift
@@ -1,24 +1,14 @@
-//
-//  ContentView.swift
-//  Playground
-//
-//  Created by 김철준 on 12/19/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        WebView(url: URL(string: "https://thedevlounge.com")!) // 여기에 원하는 웹 URL 입력
+            .edgesIgnoringSafeArea(.all) // 웹뷰가 화면 전체를 차지하도록 설정
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
