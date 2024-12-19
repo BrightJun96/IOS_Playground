e37841a0f13f0b0f16318e8d5213c59cfa66b9ce
jjalseu <cjfwns966@gmail.com>
2024-12-19 19:51:40 +0900

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by 김철준 on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
